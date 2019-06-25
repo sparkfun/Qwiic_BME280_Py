f989f8f7c27889375d1a3385e681265dd61931e6
KDB <kirk.benell@sparkfun.com>
2019-06-25 12:27:00 -0600

    cleanup - finalization

--- setup.py
+++ setup.py
@@ -69,12 +69,14 @@ setup(
         'Topic :: Software Development :: Build Tools',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'License :: OSI Approved :: MIT License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both. 
-       'Programming Language :: Python :: 2.7'
-       'Programming Language :: Python :: 3'       
+       'Programming Language :: Python :: 2.7',
+       'Programming Language :: Python :: 3.5',
+       'Programming Language :: Python :: 3.6',
+       'Programming Language :: Python :: 3.7',
     ],
 
     # What does your project relate to?
